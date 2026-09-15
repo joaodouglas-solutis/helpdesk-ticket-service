64d06786244c784d7a2bded6a01030e28cccd4f4
Joao Douglas Oliveira da Silva Santos <joao.dsantos@solutis.com.br>
2026-09-15 15:56:31 -0300

    feat: configure ticket created event publisher

--- src/main/java/com/solutis/ticketservice/config/RabbitMQConfig.java
+++ src/main/java/com/solutis/ticketservice/config/RabbitMQConfig.java
@@ -1,12 +1,9 @@
 package com.solutis.ticketservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,28 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String TICKET_EXCHANGE = "ticket.exchange";
-    public static final String TICKET_CREATED_QUEUE = "ticket.created.queue";
-    public static final String TICKET_CREATED_ROUTING_KEY = "ticket.created";
 
     @Bean
     public TopicExchange ticketExchange() {
         return new TopicExchange(TICKET_EXCHANGE);
-    }
-
-    @Bean
-    public Queue ticketCreatedQueue() {
-        return new Queue(TICKET_CREATED_QUEUE, true);
-    }
-
-    @Bean
-    public Binding ticketCreatedBinding(
-            Queue ticketCreatedQueue,
-            TopicExchange ticketExchange
-    ) {
-        return BindingBuilder
-                .bind(ticketCreatedQueue)
-                .to(ticketExchange)
-                .with(TICKET_CREATED_ROUTING_KEY);
     }
 
     @Bean
